9d34a484ae5da86c5aac3a8aa2f2a1a093ab341d
alpha4002 <51008152+alpha4002@users.noreply.github.com>
2019-05-25 13:42:00 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -646,7 +646,7 @@ client.on('message',async message => {
   let role = message.guild.roles.find('name',args) || message.guild.roles.get(args);
 
 
-  if(message.content.startsWith(prefix + "!Role")) {
+  if(message.content.startsWith(prefix + "Role")) {
     if(!args) return message.reply('اكتب اسم الرتبة');
     if(!role) return message.reply('هذه الرتبة غير موجودة');
     let iQp = new Discord.RichEmbed()
